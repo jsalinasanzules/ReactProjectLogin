1f6acb3e4dd8b754f7fbc1cede9d04e2696186fd
jsalinasanzules <jsalinasanzules@gmail.com>
2023-01-26 09:56:28 -0500

    datepicker para campo de fecha de ingreso

--- src/pages/DocumentoElectronico.js
+++ src/pages/DocumentoElectronico.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Container } from 'react-bootstrap';
 import Col from 'react-bootstrap/Col';
 import Form from 'react-bootstrap/Form';
@@ -7,12 +7,18 @@ import Tab from 'react-bootstrap/Tab';
 import Tabs from 'react-bootstrap/Tabs';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
-import Stack from 'react-bootstrap/Stack';
+import '../css/DocumentoElectronico.css';
+import DatePicker from "react-datepicker";
+import "react-datepicker/dist/react-datepicker.css";
+import 'react-datepicker/dist/react-datepicker-cssmodules.css'
+
 
 function DocumentoElectronico() {
 
+    const [startDate, setStartDate] = useState(new Date());
     return (
         <>
+        
         <Container>
             
 
@@ -25,102 +31,122 @@ function DocumentoElectronico() {
                 </Row>
 
                 <Row>
-                    <Col>
-                          <Form.Label>Fecha Emisión</Form.Label>
+                    <Col className='right-align'>
+                          <Form.Label>Fecha Emisión :</Form.Label>
                     </Col>
-                    <Col>
-                        <Form.Control type="text" placeholder="" />
+                    <Col xs={3}>
+                        <DatePicker className="mb-2 form-control"
+                                dateFormat="dd/MM/yyyy"
+                                selected={startDate}
+                                onChange={(date) => setStartDate(date)}
+                                />
                     </Col>
+                    <Col></Col>
+                    <Col></Col>
                     <Col></Col>
                     <Col></Col>
                 </Row>
 
                 <Row>
-                    <Col>
-                            <Form.Label>Tipo de Documento</Form.Label>
+                    <Col className='right-align'>
+                            <Form.Label>Tipo de Documento :</Form.Label>
                     </Col>
-                    <Col>
-                        <Form.Select defaultValue="Cliente">
+                    <Col xs={3}>
+                        <Form.Select className="mb-2" defaultValue="Cliente">
                             <option>Cliente</option>
                             <option>Proveedor</option>
                         </Form.Select>
                     </Col>
-                    <Col>
-                        <Form.Select defaultValue="Factura">
+                    <Col >
+                        <Form.Select  defaultValue="Factura">
                             <option>Factura</option>
                             <option>...</option>
                         </Form.Select>                    
                     </Col>
                     <Col>
                     </Col>
+                    <Col></Col>
+                    <Col></Col>
                 </Row>
 
                 <Row>
-                    <Col>
-                          <Form.Label>Establecimiento</Form.Label>
+                    <Col className='right-align'>
+                          <Form.Label>Establecimiento :</Form.Label>
                     </Col>
-                    <Col>
-                        <Form.Select defaultValue="Factura">
+                    <Col xs={3}>
+                        <Form.Select className="mb-2" defaultValue="Factura">
                             <option>001</option>
                         </Form.Select>  
                     </Col>
                     <Col></Col>
                     <Col></Col>
-                </Row>
-
-                <Row>
-                    <Col>
-                          <Form.Label>P.Emisión</Form.Label>
-                    </Col>
-                    <Col>
-                    <Form.Control type="text" placeholder="001" />
-                    </Col>
                     <Col></Col>
                     <Col></Col>
                 </Row>
 
                 <Row>
-                    <Col>
-                          <Form.Label># de Documento</Form.Label>
+                    <Col className='right-align'>
+                          <Form.Label>P.Emisión :</Form.Label>
                     </Col>
-                    <Col>
-                    <Form.Control type="text" placeholder="001-001-000000001" />
+                    <Col xs={3}>
+                    <Form.Control className="mb-2" type="text" placeholder="001" />
                     </Col>
+                    <Col></Col>
+                    <Col></Col>
                     <Col></Col>
                     <Col></Col>
                 </Row>
 
                 <Row>
-                    <Col>
-                          <Form.Label>Persona</Form.Label>
+                    <Col className='right-align'>
+                          <Form.Label># de Documento :</Form.Label>
                     </Col>
-                    <Col>
-                    <Form.Control type="text" placeholder="..." />
+                    <Col xs={3}>
+                    <Form.Control className="mb-2" type="text" placeholder="001-001-000000001" />
                     </Col>
+                    <Col></Col>
+                    <Col></Col>
                     <Col></Col>
                     <Col></Col>
                 </Row>
 
                 <Row>
-                    <Col>
-                          <Form.Label>Vendedor</Form.Label>
+                    <Col className='right-align'>
+                          <Form.Label>Persona :</Form.Label>
                     </Col>
-                    <Col>
-                        <Form.Select defaultValue="999999999">
+                    <Col xs={3}>
+                    <Form.Control className="mb-2" type="text" placeholder="..." />
+                    </Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
+                </Row>
+
+                <Row>
+                    <Col className='right-align'>
+                          <Form.Label>Vendedor :</Form.Label>
+                    </Col>
+                    <Col xs={3}>
+                        <Form.Select className="mb-2" defaultValue="999999999">
                             <option>-------------</option>
                         </Form.Select> 
                     </Col>
                     <Col></Col>
                     <Col></Col>
+                    <Col></Col>
+                    <Col></Col>
                 </Row>
 
                 <Row >
-                    <Col>
-                        <Form.Label>Orden Compra/Venta</Form.Label>
+                    <Col className='right-align'>
+                        <Form.Label>Orden Compra/Venta :</Form.Label>
                     </Col>
-                    <Col>
-                        <Form.Control type="file" />
+                    <Col xs={3}>
+                        <Form.Control className="mb-2" type="file" />
                     </Col>
+                    <Col></Col>
+                    <Col></Col>
                     <Col></Col>
                     <Col></Col>
                 </Row>
@@ -190,11 +216,14 @@ function DocumentoElectronico() {
         <Container >
 
         
-        <Stack direction="horizontal" gap={1}>
-                <Button variant="success" >
-                        Guardar
-                </Button>
-        </Stack>
+        <Row >
+                    <Col className='right-align'>
+                    <Button variant="success" >
+                            Guardar
+                    </Button>
+                    </Col>
+        </Row>
+        
             
         </Container>
         <br/>
